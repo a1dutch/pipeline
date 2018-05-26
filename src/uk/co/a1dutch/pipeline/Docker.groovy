c2941602582b42ea8c2e126dba540ee825ff7045
Andrew Holland <aholland@a1dutch.co.uk>
2018-05-26 21:54:42 +0100

    add default docker file

--- src/uk/co/a1dutch/pipeline/Docker.groovy
+++ src/uk/co/a1dutch/pipeline/Docker.groovy
@@ -8,11 +8,16 @@ public class Docker implements Serializable {
   }
 
   def build(Map config) {
+    String language = 'java8'
     String repository = config.repository
     String artifact = config.artifact
     String version = config.version ?: 'latest'
     boolean tagLatest = config.tagLatest == null ? true : config.tagLatest.toBoolean()
     File directory = config.directory ?: new File(".")
+
+    if (!new File(directory, 'Dockerfile').exists()) {
+      steps.writeFile(file: 'Dockerfile', text: steps.libraryResource("Dockerfile-${language}"))
+    }
 
     steps.sh("docker build -t ${repository}/${artifact}:${version} ${directory}")
     if (tagLatest && version != 'latest') {
--- test/uk/co/a1dutch/pipeline/DockerSpec.groovy
+++ test/uk/co/a1dutch/pipeline/DockerSpec.groovy
@@ -3,7 +3,8 @@ package uk.co.a1dutch.pipeline
 import spock.lang.Specification
 
 public class DockerSpec extends Specification {
-  def 'should build docker image'() {
+
+  def 'should use existing dockerfile'() {
     given:
       GroovyObjectSupport steps = GroovyMock()
       Docker docker = new Docker(steps)
@@ -11,11 +12,31 @@ public class DockerSpec extends Specification {
     when:
         docker.build(repository: "a1dutch", artifact: "test", version: "1.0.1", tagLatest: true, directory: directory)
     then:
-      1 * steps.sh("docker build -t a1dutch/test:1.0.1 test-resources/docker/valid")
-      1 * steps.sh("docker tag a1dutch/test:1.0.1 a1dutch/test:latest")
-      1 * steps.sh("docker push a1dutch/test:1.0.1")
-      1 * steps.sh("docker push a1dutch/test:latest")
-      0 * _
+      interaction {
+        dockerIsBuilt(steps, 'a1dutch/test:1.0.1', 'test-resources/docker/valid')
+        dockerTag(steps, 'a1dutch/test:1.0.1', 'a1dutch/test:latest')
+        dockerPush(steps, 'a1dutch/test:1.0.1')
+        dockerPush(steps, 'a1dutch/test:latest')
+        noMoreInteractions()
+      }
+  }
+
+  def 'should build docker image'() {
+    given:
+      GroovyObjectSupport steps = GroovyMock()
+      Docker docker = new Docker(steps)
+      File directory = new File("test-resources/docker/no-dockerfile")
+    when:
+        docker.build(repository: "a1dutch", artifact: "test", version: "1.0.1", tagLatest: true, directory: directory)
+    then:
+      interaction {
+        dockerfileIsCopied(steps)
+        dockerIsBuilt(steps, 'a1dutch/test:1.0.1', 'test-resources/docker/no-dockerfile')
+        dockerTag(steps, 'a1dutch/test:1.0.1', 'a1dutch/test:latest')
+        dockerPush(steps, 'a1dutch/test:1.0.1')
+        dockerPush(steps, 'a1dutch/test:latest')
+        noMoreInteractions()
+      }
   }
 
   def 'should tag latest by default'() {
@@ -26,11 +47,13 @@ public class DockerSpec extends Specification {
     when:
         docker.build(repository: "a1dutch", artifact: "test", version: "1.0.1", directory: directory)
     then:
-      1 * steps.sh("docker build -t a1dutch/test:1.0.1 test-resources/docker/valid")
-      1 * steps.sh("docker tag a1dutch/test:1.0.1 a1dutch/test:latest")
-      1 * steps.sh("docker push a1dutch/test:1.0.1")
-      1 * steps.sh("docker push a1dutch/test:latest")
-      0 * _
+      interaction {
+        dockerIsBuilt(steps, 'a1dutch/test:1.0.1', 'test-resources/docker/valid')
+        dockerTag(steps, 'a1dutch/test:1.0.1', 'a1dutch/test:latest')
+        dockerPush(steps, 'a1dutch/test:1.0.1')
+        dockerPush(steps, 'a1dutch/test:latest')
+        noMoreInteractions()
+      }
   }
 
   def 'should not tag latest docker image'() {
@@ -41,9 +64,11 @@ public class DockerSpec extends Specification {
     when:
         docker.build(repository: "a1dutch", artifact: "test", version: "1.0.1", tagLatest: false, directory: directory)
     then:
-      1 * steps.sh("docker build -t a1dutch/test:1.0.1 test-resources/docker/valid")
-      1 * steps.sh("docker push a1dutch/test:1.0.1")
-      0 * _
+      interaction {
+        dockerIsBuilt(steps, 'a1dutch/test:1.0.1', 'test-resources/docker/valid')
+        dockerPush(steps, 'a1dutch/test:1.0.1')
+        noMoreInteractions()
+      }
   }
 
   def 'should tag latest docker image only'() {
@@ -54,8 +79,31 @@ public class DockerSpec extends Specification {
     when:
         docker.build(repository: "a1dutch", artifact: "test", version: "latest", tagLatest: true, directory: directory)
     then:
-      1 * steps.sh("docker build -t a1dutch/test:latest test-resources/docker/valid")
-      1 * steps.sh("docker push a1dutch/test:latest")
-      0 * _
+      interaction {
+        dockerIsBuilt(steps, 'a1dutch/test:latest', 'test-resources/docker/valid')
+        dockerPush(steps, 'a1dutch/test:latest')
+        noMoreInteractions()
+      }
+  }
+
+  def noMoreInteractions() {
+    0 * _
+  }
+
+  def dockerPush(def steps, String artifact) {
+    1 * steps.sh("docker push ${artifact}")
+  }
+
+  def dockerTag(def steps, String artifact, String tag) {
+    1 * steps.sh("docker tag ${artifact} ${tag}")
+  }
+
+  def dockerIsBuilt(def steps, String artifact, String directory) {
+    1 * steps.sh("docker build -t ${artifact} ${directory}")
+  }
+
+  def dockerfileIsCopied(def steps) {
+    1 * steps.writeFile([file: "Dockerfile", text: null])
+    1 * steps.libraryResource("Dockerfile-java8")
   }
 }
